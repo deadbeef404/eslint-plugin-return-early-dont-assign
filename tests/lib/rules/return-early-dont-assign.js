5360ce74a502bee505368494b619f500ceb160e2
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-07-13 23:48:57 +0200

    fix autofix feature

--- tests/lib/rules/return-early-dont-assign.js
+++ tests/lib/rules/return-early-dont-assign.js
@@ -39,22 +39,27 @@ ruleTester.run("return-early-dont-assign", rule, {
     invalid: [
       {
           code: "function something() { var x = []; if (true) { x = x[1]; } return x; }",
+          output: "function something() { var x = []; if (true) { return x[1]; } return x; }",
           errors: [{ messageId: "expected" }]
       },
       {
           code: "function something() { var y = {a: 1}; if (true) { y = y.a; } return y; }",
+          output: "function something() { var y = {a: 1}; if (true) { return y.a; } return y; }",
           errors: [{ messageId: "expected" }]
       },
       {
           code: "function something() { var y = {a: 1}; if (true) { y = 'somethingelse'; } return y; }",
+          output: "function something() { var y = {a: 1}; if (true) { return 'somethingelse'; } return y; }",
           errors: [{ messageId: "expected" }]
       },
       {
           code: "function something() { var x = 3; if (true) { console.log('irrelevant'); x = 1; } return x; }",
+          output: "function something() { var x = 3; if (true) { console.log('irrelevant'); return 1; } return x; }",
           errors: [{ messageId: "expected" }]
       },
       {
           code: "(x) => { var y = {a: 1}; if (true) { y = 'something'; } return y; }",
+          output: "(x) => { var y = {a: 1}; if (true) { return 'something'; } return y; }",
           errors: [{ messageId: "expected" }]
       },
     ]
